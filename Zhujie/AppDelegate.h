9b9d0dee2322a5c86d52ad69640c39f77d184106
Wanli Zhu <wanli.zhu@autodesk.com>
2022-04-13 15:54:00 +0800

    Initial Commit

---
+++ Zhujie/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Zhujie
+//
+//  Created by Wanli Zhu on 2022/4/13.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Zhujie/GameViewController.h
@@ -0,0 +1,16 @@
+//
+//  GameViewController.h
+//  Zhujie
+//
+//  Created by Wanli Zhu on 2022/4/13.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import "Renderer.h"
+
+// Our macOS view controller.
+@interface GameViewController : NSViewController
+
+@end
